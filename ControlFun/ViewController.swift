065e438b8d169c19e5c499f4cb9e508360d6fca9
633squadron <633squadron@gmail.com>
2016-03-20 20:08:12 -0700

    To bottom of p. 118

--- ControlFun/ViewController.swift
+++ ControlFun/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var numberField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func textFieldDoneEditing(sender: UITextField)
+    
+    {
+        sender.resignFirstResponder()
     }
 
 
